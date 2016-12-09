545b218e75fa808975f6f2165993696b192a54d8
Chathan Driehuys <cdriehuys@gmail.com>
2016-12-08 19:33:26 -0500

    Order hunt list view by time created.

--- driehuys_christmas/scavenger_hunt/models.py
+++ driehuys_christmas/scavenger_hunt/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 
 class ScavengerHunt(models.Model):
@@ -9,8 +10,12 @@ class ScavengerHunt(models.Model):
     """
     completed = models.BooleanField(default=False)
     final_text = models.TextField(help_text='This text will be displayed after all puzzles in the hunt are completed.')
+    time_created = models.DateTimeField(default=timezone.now)
     title = models.CharField(max_length=255)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    class Meta:
+        ordering = ('time_created',)
 
     def __str__(self) -> str:
         """
