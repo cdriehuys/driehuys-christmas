d6923b3f153d26b70f563669aaef59b72ec0e124
Chathan Driehuys <cdriehuys@gmail.com>
2016-12-08 20:44:41 -0500

    Register puzzle model in admin.

--- driehuys_christmas/driehuys_christmas/settings.py
+++ driehuys_christmas/driehuys_christmas/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'adminsortable2',
     'sass_processor',
 
     'core',
--- driehuys_christmas/scavenger_hunt/admin.py
+++ driehuys_christmas/scavenger_hunt/admin.py
@@ -1,6 +1,17 @@
+from adminsortable2.admin import SortableAdminMixin
+
 from django.contrib import admin
 
 from scavenger_hunt import models
+
+
+class PuzzleAdmin(SortableAdminMixin, admin.ModelAdmin):
+    """
+    Admin for the Puzzle model.
+    """
+    fields = ('hunt', 'title', 'text', 'answer', 'completed',)
+    list_display = ('title', 'hunt', 'completed')
+    search_fields = ('hunt', 'title', 'text', 'answer')
 
 
 class ScavengerHuntAdmin(admin.ModelAdmin):
@@ -12,4 +23,5 @@ class ScavengerHuntAdmin(admin.ModelAdmin):
     search_fields = ('title', 'user')
 
 
+admin.site.register(models.Puzzle, PuzzleAdmin)
 admin.site.register(models.ScavengerHunt, ScavengerHuntAdmin)
